1725beff3fdb8f8e33c3b7d777daac189ac73bf7
Lukas <lukas.cholomskis@gmail.com>
2015-12-17 12:51:38 +0200

    no message

---
+++ hello_world.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#button1").click(function () {
+        $("#content1").slideToggle("slow");
+    });
+    $("#button2").click(function () {
+        $("#content2").slideToggle("slow");
+    });
+});
